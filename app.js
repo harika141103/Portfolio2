d913dbf4ef56022854e2ad07bf4b82bf6d434536
harika141103 <kaveripakuharika14@gmail.com>
2024-07-20 19:17:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".control").on("click", function() {
+        $(".active-btn").removeClass("active-btn");
+        $(this).addClass("active-btn");
+        $(".active").removeClass("active");
+        $("#" + $(this).data("id")).addClass("active");
+    });
+
+    $(".theme-btn").on("click", function() {
+        $("body").toggleClass("light-mode");
+    });
+});
